0dcab4eb2b46df61e90fa217b4bfe25df2179223
tecknovice <tecknovice@gmail.com>
2019-11-12 16:19:17 +0700

    update tour-of-heroes

--- tour-of-heroes/src/app/app.module.ts
+++ tour-of-heroes/src/app/app.module.ts
@@ -1,12 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { HttpClientModule }    from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
-import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
@@ -20,6 +25,10 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    ),
     AppRoutingModule
   ],
   providers: [],
--- tour-of-heroes/src/app/hero.service.ts
+++ tour-of-heroes/src/app/hero.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+// import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { MessageService } from './message.service';
 
@@ -10,19 +11,27 @@ import { MessageService } from './message.service';
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';  // URL to web api
 
-  // getHeroes(): Hero[] {
-  //   return HEROES;
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
+
+  // getHeroes(): Observable<Hero[]> {
+  //   // TODO: send the message _after_ fetching the heroes
+  //   this.messageService.add('HeroService: fetched heroes');
+  //   return of(HEROES);
   // }
+  /** GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
-    // TODO: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl)
   }
   getHero(id: number): Observable<Hero> {
     // TODO: send the message _after_ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
+  }
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }

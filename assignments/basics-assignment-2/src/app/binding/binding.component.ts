2e55aed458ef5d13e8aa41e580ed55c8e4e82640
tecknovice <tecknovice@gmail.com>
2019-12-23 15:19:07 +0700

    add basics-assignment-2

---
+++ assignments/basics-assignment-2/src/app/binding/binding.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '[app-binding]',
+  templateUrl: './binding.component.html',
+  styleUrls: ['./binding.component.css']
+})
+export class BindingComponent implements OnInit {
+
+  username = ''
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onClear(){
+    this.username = '';
+  }
+
+}
--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -10,9 +10,23 @@ export class ServersComponent {
 
   allowAddServer = false;
 
+  addServerStatus = 'No server added';
+
+  serverName;
+
   constructor(){
     setTimeout(() => {
       this.allowAddServer = true;
     }, 2000);
   }
+
+  onCreateServer(){
+    this.addServerStatus = "server " + this.serverName;
+  }
+
+  onUpdate(event: any){
+    console.log(event)
+    this.serverName = event.target.value;
+  }
+
 }
